47834bb6620e45b7aa7670cc9c353a0ae8115ebf
sachin.dangui <sachin.dangui@hmcts.net>
2026-03-23 11:36:02 +0000

    DD-41715: Add timeout and lifecycle management to validation HttpClient
    - Configure connect, socket, and connection request timeouts (5s default)
    - Use CloseableHttpClient with @PreDestroy to release resources on shutdown
    
    Change-Id: I4252676246a87c4760e53a15b825382a605b93be

--- hearing-command/hearing-command-handler/src/main/java/uk/gov/moj/cpp/hearing/command/handler/service/validation/HttpClientProducer.java
+++ hearing-command/hearing-command-handler/src/main/java/uk/gov/moj/cpp/hearing/command/handler/service/validation/HttpClientProducer.java
@@ -1,17 +1,46 @@
 package uk.gov.moj.cpp.hearing.command.handler.service.validation;
 
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class HttpClientProducer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientProducer.class);
+    private static final int DEFAULT_TIMEOUT_MS = 5000;
+
+    private CloseableHttpClient client;
+
     @Produces
     @ApplicationScoped
-    public HttpClient createHttpClient() {
-        return HttpClientBuilder.create().build();
+    public CloseableHttpClient createHttpClient() {
+        final RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(DEFAULT_TIMEOUT_MS)
+                .setSocketTimeout(DEFAULT_TIMEOUT_MS)
+                .setConnectionRequestTimeout(DEFAULT_TIMEOUT_MS)
+                .build();
+
+        client = HttpClientBuilder.create()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return client;
+    }
+
+    @PreDestroy
+    public void close() {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (final Exception e) {
+                LOGGER.warn("Failed to close HttpClient", e);
+            }
+        }
     }
 }
